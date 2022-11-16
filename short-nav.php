59babc5c3631ef58b2028b623df6a18c3b859425
prAch12020 <65279447+prAch12020@users.noreply.github.com>
2022-11-16 21:17:08 +0300

    Delete short-nav.php

--- short-nav.php
+++
@@ -1,7 +0,0 @@
-<nav class="shortNav">
-    <a href="index.php">HOME</a>
-    <a href="menu.php">MENU</a>
-    <a href="products.php">PRODUCTS</a>
-    <a href="services.php">SERVICES</a>
-    <?php require_once 'portal.php'; profile(); ?>
-</nav>
