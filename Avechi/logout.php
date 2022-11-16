87cfa5de6dec50b2536baef83a61bfac9875aba5
prAch12020 <65279447+prAch12020@users.noreply.github.com>
2022-11-16 21:41:15 +0300

    Delete Avechi directory

--- Avechi/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy();
-unset($_SESSION['shopping_cart']);
-header('location:login.php');
-?>
--- Avechi/php/session_destroy.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-session_start();
-unset($_SESSION['shopping_cart']);
-header("Location: product_category.php");
-exit();
-
-?>
